91cd47763fe0917762bd4d2e4bcff3800db78dc6
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-11-20 17:34:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,12 @@
 import express from "express";
-import { createServer } from "node:http";
 import mongoose from "mongoose";
 import cors from "cors";
-import { connectToSocket } from "./src/controllers/socketManager.js";
 import userRoutes from "./src/routes/users.routes.js";
 
 const app = express();
-const server = createServer(app);
-const io = connectToSocket(server);
 
 // Database connection string
 const CONNECTION_STRING = "mongodb://127.0.0.1:27017/ali-bhai";
-
-// Setting the port
-const PORT = process.env.PORT || 5000;
 
 // Middleware setup
 app.use(cors());
@@ -22,25 +15,20 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 // Routes
 app.use("/api/v1/users", userRoutes);
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.status(200).send("Hello world");
-})
-// Start server and connect to the database
-const start = async () => {
+});
+
+// Connect to MongoDB
+const startDb = async () => {
   try {
-    // Connect to MongoDB
     const connectionDb = await mongoose.connect(CONNECTION_STRING);
-
     console.log(`MongoDB Connected: ${connectionDb.connection.host}`);
-
-    // Start the server
-    server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   } catch (error) {
-    console.error("Error starting the server:", error);
-    process.exit(1); // Exit process with failure
+    console.error("Error connecting to MongoDB:", error);
   }
 };
+startDb();
 
-start();
+// Export the app for Vercel
+export default app;
