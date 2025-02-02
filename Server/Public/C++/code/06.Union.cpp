03e8db1b48c7204e9be29c86c8c2be102efb1473
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-02-02 21:19:18 +0530

    Update 06.Union.cpp

--- Server/Public/C++/code/06.Union.cpp
+++ Server/Public/C++/code/06.Union.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <cstring> 
+#include <cstring>
 using namespace std;
 
-struct Data {
+union Data {
     char Name[23];
-    char Course[10]; 
+    char Course[10];
 };
 
 int main() {
     Data student1;
 
     strcpy(student1.Name, "Rathod Pratik");
-    strcpy(student1.Course, "BCA");
-
     cout << "Name: " << student1.Name << endl;
+
+    // Storing Course in the same union overwrites the Name
+    strcpy(student1.Course, "BCA");
     cout << "Course: " << student1.Course << endl;
 
     return 0;
