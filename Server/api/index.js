b0d3749afa231ec9764d9f1cbd136e6772d4120e
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-30 09:29:28 +0530

    add index.js inside the api folder

--- Server/api/index.js
+++ Server/api/index.js
@@ -2,17 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
 dotenv.config();
-import CodeRoutes from "./Routes/code.route.js";
-import contactroutes from "./Routes/Contact.route.js";
-import NoteRoutes from "./Routes/notes.route.js";
-import LanguageRoutes from "./Routes/Language.route.js";
-import ProjectRoutes from "./Routes/Project.route.js";
-import SkillsRoutes from "./Routes/Skill.route.js";
-import AuthRoutes from './Routes/Auth.route.js'
-import CVRoutes from './Routes/CV.route.js'
-import AwsRoutes from "./Routes/aws.route.js";
+import CodeRoutes from "../Routes/code.route.js";
+import contactroutes from "../Routes/Contact.route.js";
+import NoteRoutes from "../Routes/notes.route.js";
+import LanguageRoutes from "../Routes/Language.route.js";
+import ProjectRoutes from "../Routes/Project.route.js";
+import SkillsRoutes from "../Routes/Skill.route.js";
+import AuthRoutes from '../Routes/Auth.route.js'
+import CVRoutes from '../Routes/CV.route.js'
+import AwsRoutes from "../Routes/aws.route.js";
 
-import { ConnectToMongoDB } from "./Controller/Connaction.js";
+import { ConnectToMongoDB } from "../Controller/Connaction.js";
 const app = express();
 app.use(
   cors({
@@ -29,7 +29,6 @@ app.get("/", (req, res) => {
   res.status(200).send("Backend is up");
 });
 
-//  app.use('/api',require('./Routes/route'))
 app.use("/code", CodeRoutes);
 app.use("/contact", contactroutes);
 app.use("/note", NoteRoutes);
@@ -41,4 +40,4 @@ app.use("/project", ProjectRoutes);
 app.use("/skills", SkillsRoutes);
 
 
-app.listen(5000, () => console.log("Server listen at 5000"));
+export default app
